6174debda769c4f033ca264d43b261598a32e228
Vinay Gupta <62155169+vinaytest75@users.noreply.github.com>
2020-12-29 22:41:39 +0530

    footer completed without modal

--- indiegogo/README.md
+++ indiegogo/README.md
@@ -4,3 +4,4 @@
 2. npm install react-router-dom
 3. npm install axios
 4. npm install styled-components
+5. npm install --save @ant-design/icons //for ant-design icons
--- indiegogo/src/Components/Footer.jsx
+++ indiegogo/src/Components/Footer.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import {
+  FacebookOutlined,
+  TwitterOutlined,
+  YoutubeFilled,
+  InstagramOutlined,
+  LinkedinFilled,
+} from "@ant-design/icons";
 
 const FooterStyle = styled.div`
   background: #f5f5f5;
@@ -23,9 +30,28 @@ const FooterCard = styled.div`
   margin-left: 100px;
 `;
 
+const BottomIcon = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  margin-left: 20%;
+
+  & div {
+    margin: 7px;
+  }
+  & button {
+    background: white;
+    border: none;
+    width: 150px;
+    margin-top: -8px;
+    padding: 10px;
+  }
+`;
+
 const Footer = () => {
   return (
     <FooterStyle>
+      {/* -----Footer Top Section ------ */}
       <div
         style={{
           display: "flex",
@@ -103,6 +129,64 @@ const Footer = () => {
             SIGN ME UP
           </button>
         </FooterCard>
+      </div>
+      <br />
+      {/* ---------Footer Bottom Section ---------- */}
+      <BottomIcon>
+        <div>
+          <button>Indian Ruppee(INR)</button>
+        </div>
+        <div>
+          <FacebookOutlined />
+        </div>
+        <div>
+          <TwitterOutlined />
+        </div>
+        <div>
+          <YoutubeFilled />
+        </div>
+        <div>
+          <InstagramOutlined />
+        </div>
+        <div>
+          <LinkedinFilled />
+        </div>
+      </BottomIcon>
+      <hr style={{ color: "#dddddd", width: "60%" }} />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          width: "60%",
+          justifyContent: "space-around",
+          marginLeft: "20%",
+        }}
+      >
+        <div>
+          <img
+            src="https://www.indiegogo.com/images/bbbImage.png"
+            alt="footerImg"
+            height="30px"
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            fontSize: "15px",
+            marginLeft: "20%",
+          }}
+        >
+          <div style={{ margin: "10px" }}>Terms of Use</div>
+          <div style={{ margin: "10px" }}>Privacy Policy</div>
+          <div style={{ margin: "10px" }}>Cookie Policy</div>
+          <div style={{ margin: "10px" }}>
+            Do Not Sell My Personal Information
+          </div>
+          <div style={{ margin: "10px" }}>
+            © 2020 Indiegogo, Inc. All Rights Reserved
+          </div>
+        </div>
       </div>
     </FooterStyle>
   );
