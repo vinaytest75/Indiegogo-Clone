8429f74e2ded5fe921244ff10dedbb41dd354a9a
Vinay Gupta <62155169+vinaytest75@users.noreply.github.com>
2020-12-30 23:05:25 +0530

    Search Bar

--- indiegogo/src/Components/Navbar.jsx
+++ indiegogo/src/Components/Navbar.jsx
@@ -7,6 +7,7 @@ import LoginPopUpModal from "./RouteComponents/LoginPopUpModal";
 import SignUp from "./RouteComponents/SignUpModal/SignUp";
 import { NavbarUser } from "./NavbarComponents/NavbarUser";
 import { UserModal } from "./NavbarComponents/UserModal";
+import { CloseOutlined, SearchOutlined } from "@ant-design/icons";
 
 const NavbarStyle = styled.div`
   display: flex;
@@ -58,6 +59,7 @@ class Navbar extends React.Component {
       modalVisibleSignUp: false,
       modalVisibleUser: false,
       isAuth: true,
+      showSearchBar: false,
     };
   }
 
@@ -73,113 +75,133 @@ class Navbar extends React.Component {
     this.setState({ modalVisibleUser });
   }
 
+  handleShowSearchBar = () => {
+    this.setState({
+      showSearchBar: !this.state.showSearchBar,
+    });
+  };
+
   render() {
-    const { isAuth } = this.state;
+    const { isAuth, showSearchBar } = this.state;
     return (
-      <NavbarStyle>
-        <div>
-          <NavLink to="/">
-            <img
-              src="/HomePage/IndiegogoLogo.png"
-              alt="IndiegogoLogo"
-              height="20px"
+      <>
+        {showSearchBar ? (
+          <div style={{ marginTop: "4px" }}>
+            <SearchOutlined style={{ fontSize: "20px" }} />
+            <input
+              placeholder="Search"
+              style={{ border: "none", width: "90%", margin: "0px 15px" }}
             />
-          </NavLink>
-          {leftLinks.map((item) => (
-            <NavLink
-              to={item.to}
-              style={{
-                textDecoration: "none",
-                padding: "10px",
-                color: "black",
-              }}
-              activeStyle={{ color: "black" }}
-            >
-              {item.title}
-            </NavLink>
-          ))}
-          <NavLink to="/search">
-            <img
-              src="/HomePage/search.png"
-              alt="searchIcon"
-              height="15px"
-              width="15px"
-            />
-          </NavLink>
-        </div>
-        <div>
-          {rightLinks.map((item) => (
-            <NavLink
-              to={item.to}
-              style={{
-                textDecoration: "none",
-                padding: "10px",
-                color: "black",
-              }}
-              activeStyle={{ color: "black" }}
-            >
-              {item.title}
-            </NavLink>
-          ))}
-          <span
-            style={{
-              border: "1px solid lightgrey",
-              width: "0px",
-            }}
-          />
-          {isAuth ? (
-            <span onClick={() => this.setModalVisibleUser(true)}>
-              <NavbarUser />
-            </span>
-          ) : (
-            <>
-              <Button onClick={() => this.setModalVisibleLogin(true)}>
-                Login
-              </Button>
-              <Button onClick={() => this.setModalVisibleSignUp(true)}>
-                Sign Up
-              </Button>
-            </>
-          )}
+            <CloseOutlined onClick={this.handleShowSearchBar} />
+          </div>
+        ) : (
+          <NavbarStyle>
+            <div>
+              <NavLink to="/">
+                <img
+                  src="/HomePage/IndiegogoLogo.png"
+                  alt="IndiegogoLogo"
+                  height="20px"
+                />
+              </NavLink>
+              {leftLinks.map((item) => (
+                <NavLink
+                  to={item.to}
+                  style={{
+                    textDecoration: "none",
+                    padding: "10px",
+                    color: "black",
+                  }}
+                  activeStyle={{ color: "black" }}
+                >
+                  {item.title}
+                </NavLink>
+              ))}
+              <NavLink to="/search">
+                <img
+                  src="/HomePage/search.png"
+                  alt="searchIcon"
+                  height="15px"
+                  width="15px"
+                  onClick={this.handleShowSearchBar}
+                />
+              </NavLink>
+            </div>
+            <div>
+              {rightLinks.map((item) => (
+                <NavLink
+                  to={item.to}
+                  style={{
+                    textDecoration: "none",
+                    padding: "10px",
+                    color: "black",
+                  }}
+                  activeStyle={{ color: "black" }}
+                >
+                  {item.title}
+                </NavLink>
+              ))}
+              <span
+                style={{
+                  border: "1px solid lightgrey",
+                  width: "0px",
+                }}
+              />
+              {isAuth ? (
+                <span onClick={() => this.setModalVisibleUser(true)}>
+                  <NavbarUser />
+                </span>
+              ) : (
+                <>
+                  <Button onClick={() => this.setModalVisibleLogin(true)}>
+                    Login
+                  </Button>
+                  <Button onClick={() => this.setModalVisibleSignUp(true)}>
+                    Sign Up
+                  </Button>
+                </>
+              )}
 
-          {/* User Modal*/}
-          <Modal
-            width={170}
-            closable={false}
-            visible={this.state.modalVisibleUser}
-            onOk={() => this.setModalVisibleUser(false)}
-            onCancel={() => this.setModalVisibleUser(false)}
-            footer={null}
-            style={{ float: "right", margin: "-40px 0px" }}
-          >
-            <UserModal />
-          </Modal>
+              {/* User Modal*/}
+              <Modal
+                width={170}
+                closable={false}
+                visible={this.state.modalVisibleUser}
+                onOk={() => this.setModalVisibleUser(false)}
+                onCancel={() => this.setModalVisibleUser(false)}
+                footer={null}
+                style={{ float: "right", margin: "-40px 0px" }}
+              >
+                <UserModal />
+              </Modal>
 
-          {/* Login Button Modal*/}
-          <Modal
-            centered
-            width={350}
-            visible={this.state.modalVisibleLogin}
-            onOk={() => this.setModalVisibleLogin(false)}
-            onCancel={() => this.setModalVisibleLogin(false)}
-            footer={null}
-          >
-            <LoginPopUpModal />
-          </Modal>
+              {/* Login Button Modal*/}
+              <Modal
+                centered
+                width={350}
+                visible={this.state.modalVisibleLogin}
+                onOk={() => this.setModalVisibleLogin(false)}
+                onCancel={() => this.setModalVisibleLogin(false)}
+                footer={null}
+              >
+                <LoginPopUpModal />
+              </Modal>
 
-          {/* Sign Up Button Modal*/}
-          <Modal
-            centered
-            width={350}
-            visible={this.state.modalVisibleSignUp}
-            onOk={() => this.setModalVisibleSignUp(false)}
-            onCancel={() => this.setModalVisibleSignUp(false)}
-            footer={null}
-          >
-            <SignUp />
-          </Modal>
-        </div>
-      </NavbarStyle>
+              {/* Sign Up Button Modal*/}
+              <Modal
+                centered
+                width={350}
+                visible={this.state.modalVisibleSignUp}
+                onOk={() => this.setModalVisibleSignUp(false)}
+                onCancel={() => this.setModalVisibleSignUp(false)}
+                footer={null}
+              >
+                <SignUp />
+              </Modal>
+            </div>
+          </NavbarStyle>
+        )}
+      </>
     );
   }
 }
