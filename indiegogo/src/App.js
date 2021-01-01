8bb34e660203508e353183c074499b622b8a51e7
Laksh-Koundinya <lakshmisn.08@gmail.com>
2021-01-01 20:23:58 +0530

    Project Ready

--- indiegogo/src/App.js
+++ indiegogo/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { ExploreProducts } from "./Components/ExploreProject/Project"
 import { Routes } from "./Components/Routes";
+import ProfilePage from "./Components/ExploreProject/ProfilePage/ProfilePage";
 
 function App() {
   return (
     <div className="App">
-      {/* <Routes /> */}
-     <ExploreProducts />
+      {/* <Routes />
+     <ExploreProducts /> */}
+     <ProfilePage />
     </div>
   );
 }
